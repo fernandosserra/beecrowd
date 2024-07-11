3be736eb8b17a59107902a48859e305d46fc8eae
Fernando Serra <fs.serra@outlook.com>
2024-07-11 18:22:58 -0300

    added 1035 and 1036

---
+++ beginner/1035.py
@@ -0,0 +1,28 @@
+'''
+Leia 4 valores inteiros A, B, C e D. A seguir, se B for maior do que C e se D for maior do que A, 
+e a soma de C com D for maior que a soma de A e B e se C e D, ambos, forem positivos e se a variável A 
+for par escrever a mensagem "Valores aceitos", senão escrever "Valores nao aceitos".
+
+Entrada
+Quatro números inteiros A, B, C e D.
+
+Saída
+Mostre a respectiva mensagem após a validação dos valores.
+Exemplo de Entrada: 
+5 6 7 8
+
+2 3 2 6
+
+Exemplo de Saída
+Valores nao aceitos
+
+Valores aceitos
+'''
+# Requesting
+a, b, c, d = map(int,(input().split(' ')))
+
+# Analysis
+if (b > c) and (d > a) and (c + d > a + b) and (c > 0) and (d > 0) and  (a % 2 == 0):
+    print('Valores aceitos')
+else:
+    print('Valores nao aceitos')
---
+++ beginner/1036.py
@@ -0,0 +1,31 @@
+'''
+Leia 3 valores de ponto flutuante e efetue o cálculo das raízes da equação de Bhaskara. 
+Se não for possível calcular as raízes, mostre a mensagem correspondente “Impossivel calcular”, caso haja uma divisão por 0 ou raiz de numero negativo.
+
+Entrada
+Leia três valores de ponto flutuante (double) A, B e C.
+
+Saída
+Se não houver possibilidade de calcular as raízes, apresente a mensagem "Impossivel calcular". 
+Caso contrário, imprima o resultado das raízes com 5 dígitos após o ponto, com uma mensagem correspondente 
+conforme exemplo abaixo. Imprima sempre o final de linha após cada mensagem.
+'''
+# imports
+from math import sqrt
+
+# requests
+a, b, c = map(float,input().split(' '))
+
+# calculating
+delta = ((b**2) - 4*(a*c))
+
+if delta > 0 and a > 0:
+    x_one = (-b + sqrt(delta)) / (2*a)
+    x_two = (-b - sqrt(delta)) / (2*a)
+    print(f'R1 = {x_one:.5f}')
+    print(f'R2 = {x_two:.5f}')
+elif delta == 0:
+    x_one = (-b + sqrt(delta)) / (2*a)
+    print(f'R1 = {x_one:.5f}')
+else:
+    print('Impossivel calcular')
