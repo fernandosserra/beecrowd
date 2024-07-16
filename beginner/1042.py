3fa4636a00768c35a9ed5368f1c8bc17b92c0143
Fernando Serra <fs.serra@outlook.com>
2024-07-16 18:23:52 -0300

    added 1042

---
+++ beginner/1042.py
@@ -0,0 +1,23 @@
+"""
+Leia 3 valores inteiros e ordene-os em ordem crescente. No final, mostre os valores em ordem crescente, 
+uma linha em branco e em seguida, os valores na sequência como foram lidos.
+
+Entrada
+A entrada contem três números inteiros.
+
+Saída
+Imprima a saída conforme foi especificado.
+"""
+# Request and assign
+usr_input = [int(x) for x in input().strip().split(' ')]
+to_sort = usr_input[:]
+
+# sorting
+to_sort.sort()
+
+# showing
+for i in range(3):
+    print(to_sort[i])
+print()
+for i in range(3):
+    print(usr_input[i])
