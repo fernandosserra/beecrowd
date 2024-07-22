afb8b5c63df0f16b3f771ccf50a3b6c6fef3f057
Fernando Serra <fs.serra@outlook.com>
2024-07-22 20:23:18 -0300

    added 1044

---
+++ beginner/1044.py
@@ -0,0 +1,17 @@
+"""
+Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos", indicando se os valores lidos são múltiplos entre si.
+
+Entrada
+A entrada contém valores inteiros.
+
+Saída
+A saída deve conter uma das mensagens conforme descrito acima.
+"""
+# requesting
+a, b = map(int,input().split())
+
+# calculating
+if b % a == 0 or a % b == 0:
+    print('Sao Multiplos')
+else:
+    print('Nao sao Multiplos')
