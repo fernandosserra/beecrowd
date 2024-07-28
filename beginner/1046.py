58e71c98a8eaf0159da08f13a9a6878851098761
Fernando Serra <fs.serra@outlook.com>
2024-07-28 15:56:43 -0300

    added 1046

---
+++ beginner/1046.py
@@ -0,0 +1,17 @@
+"""
+Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo, sabendo que o mesmo pode começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas.
+
+Entrada
+A entrada contém dois valores inteiros representando a hora de início e a hora de fim do jogo.
+
+Saída
+Apresente a duração do jogo conforme exemplo abaixo.
+"""
+# requesting
+start_hr, final_hr = map(int,input().split())
+
+# calculating
+if final_hr <= start_hr:
+    final_hr += 24
+    
+print(f'O JOGO DUROU {final_hr - start_hr} HORA(S)')
