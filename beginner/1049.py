22f7363fa5391515cd393296922166a2ebfd71ea
Fernando Serra <fs.serra@outlook.com>
2024-07-31 19:46:28 -0300

    added 1049

---
+++ beginner/1049.py
@@ -0,0 +1,32 @@
+"""
+Neste problema, você deverá ler 3 palavras que definem o tipo de animal possível segundo o esquema abaixo, da esquerda para a direita.  
+Em seguida conclua qual dos animais seguintes foi escolhido, através das três palavras fornecidas.
+"""
+# Requesting
+master_type = input()
+sub_type = input()
+diet_type = input()
+
+# Finding and showing
+if master_type == 'vertebrado':
+    if sub_type == 'ave':
+        if diet_type == 'carnivoro':
+            print('aguia')
+        else:
+            print('pomba')
+    else:
+        if diet_type == 'onivoro':
+            print('homem')
+        else:
+            print('vaca')
+else:
+    if sub_type == 'inseto':
+        if diet_type == 'hematofago':
+            print('pulga')
+        else:
+            print('lagarta')
+    else:
+        if diet_type == 'hematofago':
+            print('sanguessuga')
+        else:
+            print('minhoca')
