d9724346b56e367b21cc7049df5d86a1ddfde81a
Fernando Serra <fs.serra@outlook.com>
2024-08-06 20:19:57 -0300

    added 1059

---
+++ beginner/1059.py
@@ -0,0 +1,16 @@
+"""Faça um programa que mostre os números pares entre 1 e 100, inclusive.
+
+Entrada
+Neste problema extremamente simples de repetição não há entrada.
+
+Saída
+Imprima todos os números pares entre 1 e 100, inclusive se for o caso, um em cada linha."""
+
+# Defining
+control_var = 1
+
+# Executing
+while control_var <= 100:
+    if control_var % 2 == 0:
+        print(control_var)
+    control_var += 1
