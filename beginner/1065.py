e82cb4abc41129858b1917592e64b96795d95053
Fernando Serra <fs.serra@outlook.com>
2024-08-09 23:55:49 -0300

    Added 1065

--- beginner/1065.py
+++ beginner/1065.py
@@ -1,1 +1,15 @@
-pares
+"""
+Faça um programa que leia 5 valores inteiros. Conte quantos destes valores digitados são pares e mostre esta informação.
+
+Entrada
+O arquivo de entrada contém 5 valores inteiros quaisquer.
+
+Saída
+Imprima a mensagem conforme o exemplo fornecido, indicando a quantidade de valores pares lidos.
+"""
+values = []
+for i in range(5):
+    single_value = int(input())
+    if single_value % 2 == 0:
+        values.append(single_value)
+print(f'{len(values)} valores pares')
