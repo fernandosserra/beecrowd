4c4d53b25e93c8b2a45fb166d416770e21f5091f
Fernando Serra <fs.serra@outlook.com>
2024-10-08 17:26:12 -0300

    feat: added 1116 in C and Haskell folders

---
+++ C/beginner/1116.c
@@ -0,0 +1,32 @@
+// https://judge.beecrowd.com/pt/problems/view/1116
+// Dividindo X por Y
+
+// Libraries
+#include <stdio.h>
+
+// Main function
+int main(){
+    // Variables
+    double X, Y;
+    int usr_iterations;
+
+    // Scanning
+    scanf("%d", &usr_iterations);
+
+    // Iterating
+    for (int i = 0; i < usr_iterations; i++){
+        scanf("%lf %lf", &X, &Y);
+        if (X == 0 || Y == 0){
+            printf("divisao impossivel\n");
+        } else {
+            printf("%.1lf\n",  X / Y);
+        }
+        
+    }
+
+    // Ending code
+    return 0;
+}
+
+// By Fernando Serra
+// https://github.com/fernandosserra
---
+++ Haskell/beginner/1116.hs
@@ -0,0 +1,28 @@
+-- https://judge.beecrowd.com/pt/problems/view/1116
+-- Dividindo X por Y
+
+-- Import ReplicateM_ and Printf
+import Control.Monad (replicateM_)
+import Text.Printf
+
+-- Pair Calculation
+getPairs :: IO ()
+getPairs = do
+    -- Scanning coordinates
+    usr_pairs <- getLine    
+    let [x, y] = map read (words usr_pairs) :: [Double]
+    if x == 0 || y == 0
+        then putStrLn "divisao impossivel"
+        else if x /= 0 || y /= 0
+            then printf "%.1f\n" (x / y)
+            else return ()
+
+
+-- Main Function
+main :: IO ()
+main = do
+    usr_input <- readLn :: IO Int
+    replicateM_ usr_input getPairs
+
+-- By Fernando Serra
+-- https://github.com/fernandosserra
